0d0a34be53accac06da1a591e0f91afb6890e9fc
Samoylenko Dmitry <107771156+dmytrik@users.noreply.github.com>
2022-07-05 13:55:14 +0300

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+const galleryHtml = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href=${original}>
+      <img
+        class="gallery__image"
+        src=${preview}
+        data-source=${original}
+        alt=${description}
+      />
+    </a>
+  </div>`;
+  })
+  .join("");
+gallery.innerHTML = galleryHtml;
+const links = document.querySelectorAll(".gallery__link");
+links.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+  });
+});
+gallery.addEventListener("click", showImage);
 
-console.log(galleryItems);
+let url;
+let instance;
+
+function showImage(event) {
+  const img = event.target.classList.contains("gallery__image");
+  if (!img) {
+    return;
+  }
+  url = event.target.dataset.source;
+  instance = basicLightbox.create(`
+    <img src=${url} width="800" height="600">
+`);
+
+  instance.show();
+  window.addEventListener("keydown", escCloseModal);
+}
+function escCloseModal(event) {
+  if (event.code === "Escape") {
+    instance.close();
+    window.removeEventListener("keydown", escCloseModal);
+  }
+}
