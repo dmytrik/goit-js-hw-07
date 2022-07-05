c13b5dab0f308c8c435fcd29e1017e193ad44135
Samoylenko Dmitry <107771156+dmytrik@users.noreply.github.com>
2022-07-05 15:32:03 +0300

    add

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,16 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const galleryHtml = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href=${original}>
+    <img class="gallery__image" src=${preview} alt=${description} />
+  </a>`;
+  })
+  .join("");
+gallery.innerHTML = galleryHtml;
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
